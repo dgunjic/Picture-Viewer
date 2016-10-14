8106c6e0ba76cd20bc19674992d38b6f27d08cbd
Duško Gunjić <dgunjichtc@gmail.com>
2016-10-14 10:02:51 +0200

    zavrsen

--- PictureViewer/PictureViewer/Form1.cs
+++ PictureViewer/PictureViewer/Form1.cs
@@ -21,33 +21,47 @@ namespace PictureViewer
         {
 
         }
-
+        
         private void backgroundButton_Click(object sender, EventArgs e)
-        {
-
+        {   
+            //set background color
+            if (colorDialog1.ShowDialog() == DialogResult.OK)
+                pictureBox1.BackColor = colorDialog1.Color;
         }
 
+        
         private void showButton_Click(object sender, EventArgs e)
-        {
-            if (true)
+        {   
+            //add a picture on click
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
             {
-
+                pictureBox1.Load(openFileDialog1.FileName);
             }
         }
 
         private void closeButton_Click(object sender, EventArgs e)
-        {
+        {   
+            //close the window
+            this.Close();
 
         }
-
+        
         private void clearButton_Click(object sender, EventArgs e)
-        {
-
+        {   
+            //clear image box
+            pictureBox1.Image = null;
         }
 
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
-        {
-
+        {   
+            // If the user selects the Stretch check box, 
+            // change the PictureBox's
+            // SizeMode property to "Stretch". If the user clears 
+            // the check box, change it to "Normal".
+            if (checkBox1.Checked)
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            else
+                pictureBox1.SizeMode = PictureBoxSizeMode.Normal;
         }
     }
 }
